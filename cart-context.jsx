3646052a949d64045bb72aafbe89fe9ff2fec367
Moulisha Roy <roymoulisha709@gmail.com>
2025-09-19 17:40:08 +0530

    Add files via upload

---
+++ cart-context.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useReducer} from "react";
+import { cartReducer } from "../reducers/cartReducers";
+
+const CartContext = createContext();
+
+const CartProvider = ({children}) => {
+    const initialState = {
+        cart: []
+    }
+    const [{cart}, cartDispatch] = useReducer(cartReducer, initialState);
+    return (
+        <CartContext.Provider value={{ cart, cartDispatch }}>
+            {children}
+        </CartContext.Provider>
+    )
+
+}
+
+const useCart = () => useContext(CartContext);
+
+export {CartProvider, useCart};
---
+++ login-context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useReducer} from "react";
+import { loginReducer } from "../reducers/loginReducers";
+
+const LoginContext = createContext();
+
+const LoginProvider = ({children}) => {
+    const initialState = {
+        email: '',
+        password: ''
+    }
+    const [{email, password}, loginDispatch] = useReducer(loginReducer, initialState);
+    return (
+        <LoginContext.Provider value={{ email, password, loginDispatch }}>
+            {children}
+        </LoginContext.Provider>
+    )
+
+}
+
+const useLogin = () => useContext(LoginContext);
+
+export {LoginProvider, useLogin};
